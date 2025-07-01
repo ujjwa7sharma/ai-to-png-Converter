926ddc47d7efcbbacc9990cc29e5ab8668a1b3c5
Ujjwal Sharma <ujjwal.sharma@mathco.com>
2025-07-01 16:12:39 +0530

    Demo1

--- export.jsx
+++ export.jsx
@@ -2,11 +2,11 @@
 var exportResolution = 2400;
 app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
-// Folder paths (adjusted to Desktop)
-var aiFolder = Folder("C:\Users\UjjwalSharma\ai-to-png-converter\ai_files");
-var flatOutputFolder = Folder("C:\Users\UjjwalSharma\ai-to-png-converter\Extracted");
-var perFileFolderRoot = Folder("C:\Users\UjjwalSharma\OneDrive - TheMathCompany Private Limited\Desktop\PUMA_DAC\png_images");
-var failedLogFile = new File("C:\Users\UjjwalSharma\OneDrive - TheMathCompany Private Limited\Desktop\PUMA_DAC\failed_exports.txt");
+// Folder paths (adjusted )
+var aiFolder = Folder("C:\\Users\\UjjwalSharma\\ai-to-png-converter\\ai_files");
+var flatOutputFolder = Folder("C:\\Users\\UjjwalSharma\\ai-to-png-converter\\Extracted");
+var perFileFolderRoot = Folder("C:\\Users\\UjjwalSharma\\OneDrive - TheMathCompany Private Limited\\Desktop\\PUMA_DAC\\png_images");
+var failedLogFile = new File("C:\\Users\\UjjwalSharma\\OneDrive - TheMathCompany Private Limited\\Desktop\\PUMA_DAC\\failed_exports.txt");
 
 // Initial folder checks
 if (!aiFolder.exists) {
